487f5ba3f8031feed5f8c85f2af2a2a7534e05c4
Lucca Santos <luccacostasantosjob@gmail.com>
2022-07-17 17:56:31 -0300

    Other Components are done

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,19 @@
+import logo from "../../assets/img/logo.svg"
+import "./styles.css"
+function Header() {
+
+    return (
+        <header>
+            <div className="logo-container">
+                <img src={logo} alt="DSMeta" />
+                <h1>Registro de Divergências</h1>
+                <p>
+                    Desenvolvido por
+                    <a href="https://github.com/Lucosan">Lucca Santos</a>
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export default Header 
