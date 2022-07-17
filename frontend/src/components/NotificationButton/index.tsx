940d85e286e2e78ccb378e52d078050d3215c35c
Lucca Santos <luccacostasantosjob@gmail.com>
2022-07-17 17:32:15 -0300

    First Component Done

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import notify from "../../assets/img/notification-icon.svg"
+import "./styles.css"
+
+function NotificationButton() {
+
+    return (
+        <div className="red-btn">
+            <img src={notify} alt="Notificar" />
+        </div>
+
+    )
+}
+
+export default NotificationButton
