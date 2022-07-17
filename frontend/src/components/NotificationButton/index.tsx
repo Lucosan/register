c415308afd3b582d0efd88ed976e5c5c38a1a2ed
Lucca Santos <luccacostasantosjob@gmail.com>
2022-07-17 18:20:12 -0300

    UseState for DatePicker

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import notify from "../../assets/img/notification-icon.svg"
-import "./styles.css"
+import notify from "../../assets/img/notification-icon.svg";
+import "./styles.css";
 
 function NotificationButton() {
 
@@ -11,4 +11,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from "../NotificationButton"
 import "./styles.css"
 function SalesCard() {
+    const max = new Date();
+    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+
+    const [minDate, setMinDate] = useState(min);
+    const [maxDate, setMaxDate] = useState(max);
 
     return (
         <div className="card">
@@ -10,16 +16,16 @@ function SalesCard() {
             <div>
                 <div className="form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={minDate}
+                        onChange={(date: Date) => setMinDate(date)}
                         className="form-control"
                         dateFormat="dd/MM/yyyy"
                     />
                 </div>
                 <div className="form-control-container">
                     <DatePicker
-                        selected={new Date()}
-                        onChange={(date: Date) => { }}
+                        selected={maxDate}
+                        onChange={(date: Date) => setMaxDate(date)}
                         className="form-control"
                         dateFormat="dd/MM/yyyy"
                     />
